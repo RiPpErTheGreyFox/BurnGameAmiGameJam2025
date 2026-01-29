f372afba2212878d83f0f3664b10b132774d9e81
RiPpErTheGreyFox <ripper253@gmail.com>
2026-01-29 21:09:30 +1100

    Added static screen space drawing for testing

--- src/projectilemanager.s
+++ src/projectilemanager.s
@@ -13,6 +13,22 @@ ProjectileManagerStart:
 UpdateProjectileManager:
     ; iterate through every projectile being managed
     ; update any that require it
+
+    ; test draw a projectile to show that the manager is running
+    move.l      #projectile_gfx,a0
+    move.l      #projectile_mask,a1
+    move.l      draw_buffer,a2
+    move.w      #32,d0
+    move.w      #32,d1
+    move.w      #16,d2
+    move.w      #16,d3
+    move.w      #0,d4
+    move.w      #0,d5
+    move.w      #16,a3
+    move.w      #16,a4
+
+    bsr         draw_bob
+
     rts
 
 UpdateProjectile:
